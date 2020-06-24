7c5a1cae64c424de4b989a196fe18936e5cdeb93
Ysatuma <yssatuma@gmail.com>
2020-06-24 15:25:43 +0900

    make detail product funtion and comment out
    商品詳細情報表示のための記述をhtmlに作成し、コメントアウトした。またproductsコントローラに必要な処理を記述しコメントアウトした。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,26 @@
 class ProductsController < ApplicationController
 
+  # 商品一覧画面を表示
   def index
 
   end
 
-  def new
+  # 商品詳細画面を表示
+  def show
+    # データ入力後にコメントアウトを解除
+    @product = Product.include(:users).find(params[:id])
+    @other_products = Product.where(category_id: params[:id])
+    @parent = Category.find(params[:id]).parent
+    @grand_parent = @parent.parent
   end
 
+  # 購入画面を表示
+  def new
+
+  end
+
+  #購入処理及び購入完了画面の表示
   def create
   end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  # 数字の桁を３桁ごと','で区切って表示させる
+  def number_to_currency(price)
+    "¥#{price.to_s(:delimited, delimiter: ',')}"
+  end
+  
 end
